e351d3b17b9b8e3ca0e9da4c419dc2f12db0ec03
Pete Muir <pmuir@bleepbleep.org.uk>
2018-09-21 15:09:23 -0700

    Meow

--- main.go
+++ main.go
@@ -2,25 +2,25 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
+var gen *rand.Rand = rand.New(rand.NewSource(makeTimestamp()))
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example"
+	imgNo := gen.Intn(300)
 
-	from := ""
-	if r.URL != nil {
-		from = r.URL.String()
-	}
-	if from != "/favicon.ico" {
-		log.Printf("title: %s\n", title)
-	}
-
-	fmt.Fprintf(w, "Hello from:  "+title+"\n")
+	fmt.Fprintf(w, "<html><body><img src=\"https://www.catgifpage.com/gifs/%d.gif\"></body></html>\n", imgNo)
 }
 
 func main() {
+
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
+}
+
+func makeTimestamp() int64 {
+	return time.Now().UnixNano() / int64(time.Millisecond)
 }
